bd82497ed50d103e2b79c14ffda49edb23342e24
dolly.azur <azurgrisby@hotmail.fr>
2024-12-10 14:07:39 +0100

    premier essai mise en place header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import logo from "./logo.svg";
+import image from "./images/Header/Desktop/Screen=Desktop, Active=Accueil.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={image}
+          className="header-image"
+          alt="représentation du header"
+        />
         <p>da web inside</p>
         <a
           className="App-link"
