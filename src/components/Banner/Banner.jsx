265221788ead401741663f15ffca24b83c02d098
dolly.azur <azurgrisby@hotmail.fr>
2024-12-10 17:44:55 +0100

    début correction css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../../images/images_sources/Image_source_1.png";
+import "../../components/Banner/banner.scss";
 
 function Banner() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../../images/Header-img/Desktop/Active_Accueil.png";
+import "../../components/Header/header.scss";
 
 function Header() {
   return (
