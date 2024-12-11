cea5cf6a01153eb820e1f249dd21d861e75254fd
dolly.azur <azurgrisby@hotmail.fr>
2024-12-11 11:07:50 +0100

    premiere creation routage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-//import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Banner from "./components/Banner/Banner.jsx";
 
-//const router = createBrowserRouter([ //dans le tableau, on va préciser les différentes routes
-//{ //définir les différentes routes
-//path: '/'//défini par propriété path qui va dénir le chemin, ex page d'accueil
-//element: <div>Page d'accueil</div> //element à rendre lorsque cette page est chargé, il suffit de mettre du jsx
-//}
+const router = createBrowserRouter([
+  //dans le tableau, on va préciser les différentes routes
+  {
+    //définir les différentes routes
+    path: "/home", //défini par propriété path qui va dénir le chemin, ex page d'accueil
+    element: <div>Page d'accueil</div>, //element à rendre lorsque cette page est chargé, il suffit de mettre du jsx
+  },
 
-//])
+  {
+    path: "/fiche_logement",
+    element: <div>Fiche des logements</div>,
+  },
+
+  {
+    path: "/a_propos",
+    element: <div>A propos</div>,
+  },
+
+  {
+    path: "/erreur",
+    element: <div>Erreur 404</div>,
+  },
+]);
 
 function App() {
   return (
