e1dcc9c19ca9340ae5ac88255e43ec086206f8e3
dolly.azur <azurgrisby@hotmail.fr>
2024-12-11 10:32:55 +0100

    installation vite latest + essai createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+//import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Banner from "./components/Banner/Banner.jsx";
+
+//const router = createBrowserRouter([ //dans le tableau, on va préciser les différentes routes
+//{ //définir les différentes routes
+//path: '/'//défini par propriété path qui va dénir le chemin, ex page d'accueil
+//element: <div>Page d'accueil</div> //element à rendre lorsque cette page est chargé, il suffit de mettre du jsx
+//}
+
+//])
 
 function App() {
   return (
