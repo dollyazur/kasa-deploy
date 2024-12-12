c12f343eb923a3e97ba992e624a0d8efb57b9aa0
dolly.azur <azurgrisby@hotmail.fr>
2024-12-12 17:41:23 +0100

    création d'un composant à part pour le routeur

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import "./App.css";
-import Home from "./pages/Home.jsx";
-import FicheLogement from "./pages/FicheLogement.jsx";
-import Propos from "./pages/Propos.jsx";
-
-import Erreur from "./pages/Erreur.jsx";
-
-const router = createBrowserRouter([
-  //dans le tableau, on va préciser les différentes routes
-  {
-    //définir les différentes routes
-    path: "/", //défini par propriété path qui va dénir le chemin, ex page d'accueil
-    element: <Home />, //element à rendre lorsque cette page est chargé, il suffit de mettre du jsx
-  },
-
-  {
-    path: "/fichelogement",
-    element: <FicheLogement />,
-  },
-
-  {
-    path: "/propos",
-    element: <Propos />,
-  },
-
-  {
-    path: "*", //route generique pour les erreurs
-    element: <Erreur />,
-  },
-]);
+import AppRouter from "./pages/routes/Routeur.jsx";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <AppRouter />;
 }
 
 export default App;
---
+++ src/pages/routes/Routeur.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "../Home.jsx";
+import Propos from "../Propos.jsx";
+import FicheLogement from "../FicheLogement.jsx";
+import Erreur from "../Erreur.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/fichelogement",
+    element: <FicheLogement />,
+  },
+  {
+    path: "/Propos",
+    element: <Propos />,
+  },
+  {
+    path: "*",
+    element: <Erreur />,
+  },
+]);
+
+function AppRouter() {
+  return <RouterProvider router={router} />;
+}
+
+export default AppRouter;
