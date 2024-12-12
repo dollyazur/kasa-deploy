83cf2080a1c61a0584a657486a0e8d1277987d07
dolly.azur <azurgrisby@hotmail.fr>
2024-12-12 12:31:22 +0100

    debut création page apropos, probleme pour appliquer le sass banner

--- src/pages/APropos.jsx
+++ src/pages/APropos.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Header from "../components/Header/Header.jsx";
+import "../components/Banner/banner.scss";
+import AProposimg from "../images/images_sources/Image_source_2.png";
 import Footer from "../components/Footer/Footer.jsx";
 
 function APropos() {
   return (
     <div>
       <Header />
-
+      <img src={AProposimg} alt="paysage de la banniere" />
       <Footer />
     </div>
   );
