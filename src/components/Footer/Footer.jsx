7982d03d656ed973a51c803e7405d9fc5747d46b
dolly.azur <azurgrisby@hotmail.fr>
2024-12-30 12:33:47 +0100

    correction responsive footer, et collaspses fiche logement. a refaire responsive tag, rating, name host et img host

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import image from "../../images/Footer/Size=Desktop.png";
+import image_desktop from "../../images/Footer/Size=Desktop.png";
+import image_mobile from "../../images/Footer/Size=Mobile.png";
 import "../../components/Footer/footer.scss";
 
 function Footer() {
   return (
     <div className="footer">
-      <img src={image} alt="footer kasa fond noir" />
+      <div className="footer_desktop">
+        <img src={image_desktop} alt="footer_desktop kasa fond noir" />
+      </div>
+      <div className="footer_mobile">
+        <img src={image_mobile} alt="footer_mobile kasa fond noir" />
+      </div>
     </div>
   );
 }
