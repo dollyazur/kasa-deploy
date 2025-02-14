34750aad41049e2618a4fcacacd26a90071d89c8
dollyazur <azurgrisby@hotmail.fr>
2025-02-14 15:02:19 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kasa/',
   server: {
     port: 3000, // Définit le port à 3000
   },
