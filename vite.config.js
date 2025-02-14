d1c39369b167584b25602ea29dcc60a8f766d32e
dollyazur <azurgrisby@hotmail.fr>
2025-02-14 15:23:28 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kasa-deploy",
   plugins: [react()],
-  base: '/kasa/',
+  
   server: {
     port: 3000, // Définit le port à 3000
   },
